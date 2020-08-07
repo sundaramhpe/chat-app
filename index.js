5afd09d62af6ab55532b1ef49b584d9cefeea035
sundaramhpe <sunram_c@hotmail.com>
2020-08-07 21:14:50 +0530

    Completed the socket server

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const getVisitors =()=>{
+  // creacte clients this will give us object of connected sokets
+  let clients =io.sockets.clients().connected;
+  //Object.value function grab all values from clients object 
+  //And create an array for using  those values 
+  //and return us all connected sockets
+  let sockets = Object.values(clients);
+  ///create array user
+   let users = sockets.map(s=>s.user);
+   return users ;
+}
+
+//  create another function to emits all users
+ const emitVisitors =()=>{
+   io.emit('Visitors',getVisitors());
+ };
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -14,11 +30,15 @@ io.on('connection', (socket) => {
 
   // pass new visitor 
   socket.on('new_visitor', user => {
-    console.log('new_visitor',user)
-    socket.user = user
+    console.log('new_visitor',user);
+    socket.user = user;
+    // whenever new user is connected here emit this evets
+    emitVisitors();
   })
 
   socket.on('disconnect', function () {
+    //when ever new user disconnecting fire the events again
+    emitVisitors();
     console.log('User Disconnected ')
   });
 });
