629a09729595128230cd76573372b7bb5cfbb532
sundaramhpe <sunram_c@hotmail.com>
2020-08-07 20:14:39 +0530

    new visitor created

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
+const port=6600;
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+
+  // pass new visitor 
+  socket.on('new_visitor', user => {
+    console.log('new_visitor',user)
+    socket.user = user
+  })
+
+  socket.on('disconnect', function () {
+    console.log('User Disconnected ')
+  });
+});
+
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
